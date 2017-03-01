3ebabf2821335275722fe6322f77b1dee6be0a79
Michael Bebenita <mbebenita@gmail.com>
2017-03-01 12:14:30 -0800

    Add record button and wave visualization.

--- Voicebank/Voicebank/ViewController.swift
+++ Voicebank/Voicebank/ViewController.swift
@@ -7,16 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
+import Accelerate
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LongPressRecordButtonDelegate {
 
-    @IBOutlet var recordButton: UIButton!
+    // @IBOutlet var recordButton: UIButton!
+    @IBOutlet weak var recordButton: LongPressRecordButton!
     var recorder: Recorder!
+    var engine:AVAudioEngine = AVAudioEngine()
 
+    @IBOutlet weak var leftWaveView: SwiftSiriWaveformView!
+    @IBOutlet weak var rightWaveView: SwiftSiriWaveformView!
+    
+    var timer:Timer?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         recorder = Recorder()
+        startWaveView()
+        recordButton.delegate = self
+        
+        // Flip the view around.
+        self.leftWaveView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI))
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,17 +40,57 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func longPressRecordButtonDidStartLongPress(_ button: LongPressRecordButton) {
+        recordTapped()
+    }
+    
+    func longPressRecordButtonDidStopLongPress(_ button: LongPressRecordButton) {
+        recordTapped()
+    }
+    
     @IBAction func recordTapped() {
+        print("recordTapped")
         if (self.recorder != nil){
             if (!self.recorder.isRecording()){
                 self.recorder.startRecording()
-                recordButton.setTitle("Tap to Stop", for: .normal)
+                amplitudeDelta = 0.01
             } else {
                 self.recorder.stopRecording()
-                recordButton.setTitle("Record", for: .normal)
+                amplitudeDelta = -0.01
             }
         }
     }
     
+    var amplitude:Float = 0
+    var amplitudeDelta:Float = 0
+    var amplitudeMultiplier:Float = 1
+    
+    internal func waveViewTick(_:Timer) {
+        amplitude = Float.maximum(0, Float.minimum(amplitude + amplitudeDelta, 0.1))
+        let value = CGFloat(amplitude * amplitudeMultiplier)
+        self.leftWaveView.amplitude = value
+        self.rightWaveView.amplitude = value
+    }
+    
+    func stopWaveViewTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    func startWaveView() {
+        timer = Timer.scheduledTimer(timeInterval: 0.013, target: self, selector: #selector(ViewController.waveViewTick(_:)), userInfo: nil, repeats: true)
+        
+        let input = engine.inputNode!
+        input.installTap(onBus: 0, bufferSize: 0, format: input.inputFormat(forBus: 0)) { (buffer: AVAudioPCMBuffer!, time: AVAudioTime!) -> Void in
+            if let floatChannelData = buffer.floatChannelData {
+                let stride = buffer.stride
+                let length = Int(buffer.frameLength)
+                var avgValue: Float = 0
+                vDSP_meamgv(floatChannelData.pointee, stride, &avgValue, vDSP_Length(length))
+                let newAmplitudeMultiplier = 1 + avgValue * 100
+                self.amplitudeMultiplier += (newAmplitudeMultiplier - self.amplitudeMultiplier) * 0.2
+            }
+        }
+        try! engine.start()
+    }
 }
 
