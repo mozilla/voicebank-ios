0b9fcc67eb3374d996d25b0f5c2ae66131a26f76
Andre Natal <anatal@gmail.com>
2017-03-01 09:37:52 -0800

    - Adding Textview - getSentences method - Sending the right content-type (mp4a) to the server

--- Voicebank/Voicebank/WSComm.swift
+++ Voicebank/Voicebank/WSComm.swift
@@ -11,20 +11,19 @@ import Foundation
 class WSComm: NSObject, URLSessionDelegate, URLSessionTaskDelegate, URLSessionDataDelegate {
    
     var responseData = NSMutableData()
-    
+    let webserviceHostname = "http://127.0.0.1:8000"
     override init(){
         
     }
     
     func uploadAudio(audioFile : URL) {
-
+        
         do {
-            
             let data: NSData = try NSData(contentsOfFile: audioFile.path)
-            
-            let request = NSMutableURLRequest(url: NSURL(string: "https://127.0.0.1:4343/upload/s1245/") as! URL)
+            let request = NSMutableURLRequest(url: NSURL(string: "\(self.webserviceHostname)/upload/s1245/") as! URL)
             request.httpMethod = "POST"
             request.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
+            request.setValue("audio/mp4a", forHTTPHeaderField: "content-type")
             
             let configuration = URLSessionConfiguration.default
             let session = URLSession(configuration: configuration, delegate: self, delegateQueue: OperationQueue.main)
@@ -34,11 +33,24 @@ class WSComm: NSObject, URLSessionDelegate, URLSessionTaskDelegate, URLSessionDa
         } catch let error as NSError{
             print("Error: \(error)")
         }
+    
     }
     
+    func getSentences() {
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "\(self.webserviceHostname)/sentences.json") as! URL)
+        request.httpMethod = "GET"
+        request.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration, delegate: self, delegateQueue: OperationQueue.main)
+        let task = session.uploadTask(withStreamedRequest: request as URLRequest)
+        task.resume()
+    
+    }
     
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
         self.responseData.append(data as Data)
+        NSLog(String(data:self.responseData as Data, encoding: String.Encoding.utf8)!)
     }
     
     
